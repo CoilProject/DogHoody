3961af9c4fa772b3048c9755dda468f3778535ef
MoonSlime <sck3578@naver.com>
2016-08-03 23:19:27 +0900

    마커추

--- app/src/main/java/com/hoody/doghoody/MapActivity.java
+++ app/src/main/java/com/hoody/doghoody/MapActivity.java
@@ -5,9 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class MapActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private int tag=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +21,7 @@ public class MapActivity extends AppCompatActivity {
 
 
         //다음지도 사용
-        MapView mapView = new MapView(this);
+        mapView = new MapView(this);
         mapView.setDaumMapApiKey("61cf90638e47b3e02de4dd54abd50797");
 
         ViewGroup mapViewContainer = (ViewGroup)findViewById(R.id.map_view);
@@ -26,8 +31,27 @@ public class MapActivity extends AppCompatActivity {
         //.POIItemEventListener
         //.OpenAPIKeyAuthenticationResultListener
 
+
+        //중심점변경
+        //mapView.setMapCenterPoint(MapPoint,true);
+
+
+        //마커 생성 및 설정
+        addMarker("시청역",37.5657125,126.9771195);
+
         RelativeLayout container = (RelativeLayout) findViewById(R.id.map_view);
         container.removeAllViews();
         container.addView(mapView);
     }
+
+    private void addMarker(String name,double latitude,double longitude){
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName(name);
+        marker.setTag(tag++);
+        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(latitude,longitude));
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        mapView.addPOIItem(marker);
+    }
+
 }
